a19d3d331b10183d3b36c3f50e6cee6931dc29de
Michal Paluchowski <michal@paluchowski.com>
2014-04-13 11:28:16 +0200

    Hashed links can now be loaded from bookmarks

--- lib.js
+++ lib.js
@@ -5,11 +5,35 @@ wishlist.Main = function() {
 	var init = function() {
 		$( 'h1' ).click( homepageDisplay );
 		$( '#wishlist-selection li' ).click( toggleWishlistDisplay )
+
+		loadInitialPage();
 	},
+
+	loadInitialPage = function() {
+		if ( '' === window.location.hash
+			|| 0 === $( window.location.hash ).length ) {
+			return;
+		}
+
+		$( '.wishlist' ).hide();
+		$( '#home ' ).hide();
+		$( window.location.hash ).show();
+
+		var wishlistId = window.location.hash.replace(
+			/^#wishlist\-(.*)$/g, "$1"
+			);
+		if ( '' !== wishlistId ) {
+			$( '#wishlist-selection li' ).removeClass( 'selected' );
+			$( 'li[data-id=' + wishlistId + ']', '#wishlist-selection' ).addClass( 'selected' );
+		}
+	}
 
 	homepageDisplay = function( event ) {
 		$( '.wishlist' ).hide();
+		$( '#wishlist-selection li' ).removeClass( 'selected' );
 		$( '#home ' ).show();
+
+		history.replaceState( {}, "", '/' );
 	},
 
 	toggleWishlistDisplay = function( event ) {
@@ -20,6 +44,8 @@ wishlist.Main = function() {
 		
 		$( '#wishlist-selection li' ).removeClass( 'selected' );
 		$( this ).addClass( 'selected' );
+
+		history.pushState( {}, "", '#wishlist-' + $(this).attr('data-id') );
 	}
 
 	return {
