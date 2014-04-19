baab035178e3c04e7f4f5b8c650d369f30d00732
Michal Paluchowski <michal@paluchowski.com>
2014-04-19 19:43:17 +0200

    Adding a Controller
    This one will manage all actions by the user. At first, these are claims
    for items.

---
+++ lib/Controller.php
@@ -0,0 +1,32 @@
+<?php
+namespace lib;
+
+class Controller {
+
+	public function processAction() {
+		if (!isset($_REQUEST['action']))
+			die('No action, no reaction');
+
+		$actionFunction = 'action_' . $_REQUEST['action'];
+		if (method_exists($this, $actionFunction))
+			$this->$actionFunction($_REQUEST);
+
+		if (!$this->isAjax) {
+			header("Location: /");
+		}
+	}
+
+	public function action_claim($params) {
+		if (!isset($params['id']))
+			die('ID missing');
+
+		$claimManager = new \lib\ClaimManager();
+		$claimManager->addClaim($params['id']);
+	}
+
+	private function isAjax() {
+		return !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+				&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+	}
+
+}
