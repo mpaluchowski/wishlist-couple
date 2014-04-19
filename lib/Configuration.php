d8949cb890147b2875aa3e1929ae8acb846bb425
Michal Paluchowski <michal@paluchowski.com>
2014-04-19 18:43:08 +0200

    Adding a configuration wrapper.

---
+++ lib/Configuration.php
@@ -0,0 +1,58 @@
+<?php
+namespace lib;
+
+class Configuration {
+
+	private $data = [];
+
+	public function __construct($file = 'data\config.inc.php') {
+		$this->load($file);
+	}
+
+	public function load($file) {
+		if (!is_file($file)) {
+			die('You need to create the file config.inc.php in the \data\ folder.');
+		}
+		include_once($file);
+
+		$this->data['language'] = $language;
+		$this->data['title'] = $title;
+		$this->data['quote'] = $quote;
+		$this->data['intro'] = $intro;
+		$this->data['names'] = $names;
+
+		$this->data['wishlists'] = $wishlists;
+
+		foreach ($this->data['wishlists'] as $wishlist => $items) {
+			foreach ($items as $key => $item) {
+				$this->data['wishlists'][$wishlist][$key]['id'] =
+						md5($wishlist . $item['name']);
+			}
+		}
+	}
+
+	public function getLanguage() {
+		return $this->data['language'];
+	}
+
+	public function getTitle() {
+		return $this->data['title'];
+	}
+
+	public function getQuote() {
+		return $this->data['quote'];
+	}
+
+	public function getIntro() {
+		return $this->data['intro'];
+	}
+
+	public function getName($index) {
+		return $this->data['names'][$index];
+	}
+
+	public function getWishlists() {
+		return $this->data['wishlists'];
+	}
+
+}
