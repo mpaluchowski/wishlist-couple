3ac262ecb6b31471256abebd403377c4e74e9c67
Michal Paluchowski <michal@paluchowski.com>
2014-04-19 18:43:24 +0200

    Using the new Configuration wrapper
    Includes autoloading.

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
-include("./data/config.inc.php");
+spl_autoload_extensions('.php');
+spl_autoload_register();
+
+$config = new \lib\Configuration();
 
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $language ?>">
+<html lang="<?php echo $config->getLanguage() ?>">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo htmlspecialchars( $title ) ?></title>
+	<title><?php echo htmlspecialchars( $config->getTitle() ) ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="noindex, nofollow, noarchive, noimageindex">
 
@@ -29,7 +32,7 @@ include("./data/config.inc.php");
 
 	<nav>
 		<ul id="wishlist-selection">
-			<li data-id="<?php echo $names[0]['id'] ?>"><?php echo $names[0]['name'] ?></li><li data-id="<?php echo $names[1]['id'] ?>"><?php echo $names[1]['name'] ?></li>
+			<li data-id="<?php echo $config->getName(0)['id'] ?>"><?php echo $config->getName(0)['name'] ?></li><li data-id="<?php echo $config->getName(1)['id'] ?>"><?php echo $config->getName(1)['name'] ?></li>
 		</ul>
 	</nav>
 </header>
@@ -38,24 +41,24 @@ include("./data/config.inc.php");
 
 <section id="home" class="page">
 	<blockquote>
-		<?php echo htmlspecialchars( $quote['text'] ) ?>
-		<cite><?php echo htmlspecialchars( $quote['cite'] ) ?></cite>
+		<?php echo htmlspecialchars( $config->getQuote()['text'] ) ?>
+		<cite><?php echo htmlspecialchars( $config->getQuote()['cite'] ) ?></cite>
 	</blockquote>
 
-	<?php if ( is_array( $intro['text'] ) ):
-		  foreach ( $intro['text'] as $paragraph ): ?>
+	<?php if ( is_array( $config->getIntro()['text'] ) ):
+		  foreach ( $config->getIntro()['text'] as $paragraph ): ?>
 	<p><?php echo $paragraph ?></p>
 	<?php endforeach;
 		  else: ?>
-	<p><?php echo $intro['text'] ?></p>
+	<p><?php echo $config->getIntro()['text'] ?></p>
 	<?php endif; ?>
-	<p class="signature"><?php echo htmlspecialchars( $intro['signature'] ) ?></p>
+	<p class="signature"><?php echo htmlspecialchars( $config->getIntro()['signature'] ) ?></p>
 </section>
 
-<?php foreach ($wishlists as $wishlist => $items): ?>
+<?php foreach ($config->getWishlists() as $wishlist => $items): ?>
 <section id="wishlist-<?php echo $wishlist ?>" class="wishlist">
 	<ul>
-<?php foreach ($items as $item): ?><li><article>
+<?php foreach ($items as $item): ?><li id="item-<?php echo $item['id'] ?>"><article>
 			<img src="<?php echo $item['image'] ?>" alt="<?php echo $item['name'] ?>">
 			<h2><?php echo $item['name'] ?></h2>
 
